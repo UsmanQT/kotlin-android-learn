463d8a6e106b3bbb46361ac41ca488109db0fb37
usmanqt <qureshiu@mail.gvsu.edu>
2024-05-14 17:09:40 -0400

    Addd another composable function called Massage and centered the text.

--- app/src/main/java/com/example/starter/MainActivity.kt
+++ app/src/main/java/com/example/starter/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.starter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,7 +25,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Usman")
-
+                    Message()
                 }
             }
         }
@@ -33,12 +35,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
+    Surface(color = Color.White) {
         Text(
             text = "Hi, my name is $name!",
             modifier = modifier.padding(24.dp)
         )
     }
+}
+@Composable
+fun Message(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.padding(24.dp),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Surface(color = Color.Yellow) {
+            Text(
+                text = "This is my first android app. I will be adding othr components and styling them.",
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
