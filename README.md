85b7edd86bcc06d44aafaff02284943cb122bd78
usmanqt <qureshiu@mail.gvsu.edu>
2024-05-15 15:23:25 -0400

    Added Column inside the GreetingText to display the greeting message and the signature

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## My Android Journey
 
 ## Introduction
-This is a starter Android app project that demonstrates basic usage of Jetpack Compose for building UI components. I have covered learning fundamentals of Kotlin language and setting up android studio for development with kotlin by following the android development course from Google.
+This is a birthday card Android app project that demonstrates basic usage of Jetpack Compose for building UI components. I have covered learning fundamentals of Kotlin language and setting up android studio for development with kotlin by following the android development course from Google.
 
 Link to the course: [android-basics-compose](https://developer.android.com/courses/android-basics-compose/course)
 
@@ -13,11 +13,9 @@ To get started with this project, follow these steps:
 3. Build and run the project on an emulator or a physical device.
 
 ## Features
-- **Greeting**: Displays a greeting message with a customizable name.
-- **Message**: Displays a message with a yellow background.
+- **GreetingText**: Displays a greeting happy birthday message with a name.
+- **Signature**: Displays the signature of person the card is from
 
-## Usage
-You can customize the name in the greeting message by modifying the `name` parameter in the `Greeting` composable.
 
 ## Dependencies
 This project uses the following dependencies:
--- app/src/main/java/com/example/birthdaycard/BirthdayCardActivity.kt
+++ app/src/main/java/com/example/birthdaycard/BirthdayCardActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.birthdayCard.ui.theme.BirthdayCardTheme
 
 class BirthdayCardActivity : ComponentActivity() {
@@ -24,44 +27,35 @@ class BirthdayCardActivity : ComponentActivity() {
             BirthdayCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Usman")
-                    Message()
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.White) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
         Text(
-            text = "Hi, my name is $name!",
-            modifier = modifier.padding(24.dp)
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            text = message
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp
         )
     }
-}
-@Composable
-fun Message(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier.padding(24.dp),
-        contentAlignment = Alignment.Center
-    ) {
-
-        Surface(color = Color.Yellow) {
-            Text(
-                text = "This is my first android app. I will be adding other components and styling them.",
-            )
-        }
-    }
 
 }
+@Preview(
+    showBackground = false,
+    showSystemUi = true,
+)
 
-@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BirthdayCardTheme {
-        Greeting("Usman")
+        GreetingText(message = "Happy Birthday Usman", from = "From Qureshi")
     }
 }
