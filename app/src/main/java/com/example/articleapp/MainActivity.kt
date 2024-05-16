320213eb31ac639b5967923b8da363d61f010a20
usmanqt <qureshiu@mail.gvsu.edu>
2024-05-16 16:37:10 -0400

    added heading text and the body paragraph 1 text

--- app/src/main/java/com/example/articleapp/MainActivity.kt
+++ app/src/main/java/com/example/articleapp/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.articleapp.ui.theme.ArticleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +31,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WideImage()
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        WideImage()
+                        AllTexts(textHeading = "Jetpack Compose tutorial",
+                            textBody = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                            modifier = Modifier)
+                    }
+
                 }
             }
         }
@@ -38,6 +53,24 @@ fun WideImage(modifier: Modifier = Modifier) {
         painter = image,
         contentDescription = null
     )
+}
+
+@Composable
+fun AllTexts(textHeading: String, textBody: String, modifier: Modifier) {
+    Column(
+        modifier =  modifier,
+    ) {
+        Text(
+            text = textHeading,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp),
+        )
+        Text(
+            text = textBody,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
