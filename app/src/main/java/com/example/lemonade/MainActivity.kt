9bc570d17b9fad7d4a75d7c64a86a8d19c5f722e
usmanqt <qureshiu@mail.gvsu.edu>
2024-05-21 17:00:56 -0400

    added basic state for changing the elements on the screen when they are tapped

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -22,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,6 +33,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +58,7 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LemonApp(modifier: Modifier = Modifier) {
-
+    var screenNumber by remember { mutableStateOf(1)}
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -66,26 +72,48 @@ fun LemonApp(modifier: Modifier = Modifier) {
             )
         },
     ) { innerPadding ->
-        Column (
-            modifier = modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Box (
-                modifier = modifier
-                    .background(color = Color.Yellow)
-                    .padding(innerPadding)
-
-            ){
-                Image(
-                    painter = painterResource(id = R.drawable.lemon_tree),
-                    contentDescription = "tree"
-                )
+        when (screenNumber) {
+            1 -> Surface(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                Column(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = { screenNumber++ }) {
+                        Image(
+                            painter = painterResource(id = R.drawable.lemon_tree),
+                            contentDescription = "tree"
+                        )
+                    }
+                    Spacer(modifier = modifier.height(20.dp))
+                    Text(stringResource(id = R.string.lemon_tree))
+                }
             }
-            Spacer(modifier = modifier.height(20.dp))
-            Text(stringResource(id = R.string.lemon_tree))
+            2 -> Surface(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                Column(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = { screenNumber++ }) {
+                        Image(
+                            painter = painterResource(id = R.drawable.lemon_squeeze),
+                            contentDescription = "squeeze"
+                        )
+                    }
+                    Spacer(modifier = modifier.height(20.dp))
+                    Text(stringResource(id = R.string.lemon_squeeze))
+                }
+            }
+            else -> Text(text="else")
         }
+
     }
 
 }
