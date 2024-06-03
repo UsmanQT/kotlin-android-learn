e19ba064fe78269ff27515f44f4dc5b172b1a7b6
usmanqt <qureshiu@mail.gvsu.edu>
2024-06-03 12:25:08 -0400

    starter code for affirmations app

---
+++ app/src/main/java/com/example/affirmationapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.affirmationapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.affirmationapp.ui.theme.AffirmationAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AffirmationAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AffirmationsLayoutApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AffirmationsLayoutApp(
+) {
+    Text(text = "Hey")
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AffirmationAppTheme {
+        AffirmationsLayoutApp()
+    }
+}
