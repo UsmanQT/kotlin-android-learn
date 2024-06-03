b69c1cfc436c2d0b77359281e4b21ad9730fb2a4
usmanqt <qureshiu@mail.gvsu.edu>
2024-06-03 14:44:21 -0400

    Complete code for affirmations app. scrollable using lazy column

--- app/src/main/java/com/example/affirmationapp/MainActivity.kt
+++ app/src/main/java/com/example/affirmationapp/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,10 +24,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.affirmationapp.data.Datasource
+import androidx.compose.foundation.lazy.items
 import com.example.affirmationapp.model.Affirmation
 import com.example.affirmationapp.ui.theme.AffirmationAppTheme
 
@@ -42,9 +52,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AffirmationsLayoutApp(
-) {
-    Text(text = "Hey")
+fun AffirmationsLayoutApp() {
+        AffirmationList(affirmationList = Datasource().loadAffirmations())
+
 
 }
 
@@ -69,6 +79,18 @@ fun AffirmationCard (affirmation: Affirmation, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+                )
+
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
