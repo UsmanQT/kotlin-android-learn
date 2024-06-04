e435c5e48cdbccaf52b039ac007fe17bdad66644
usmanqt <qureshiu@mail.gvsu.edu>
2024-06-04 12:56:01 -0400

    grid app complete

--- app/src/main/java/com/example/gridapp/MainActivity.kt
+++ app/src/main/java/com/example/gridapp/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -35,6 +39,7 @@ import com.example.gridapp.data.DataSource
 import com.example.gridapp.data.Topic
 import com.example.gridapp.ui.theme.GridAppTheme
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +48,9 @@ class MainActivity : ComponentActivity() {
             GridAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
                 ) {
                     GridApp()
                 }
@@ -53,33 +61,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GridApp() {
-    ItemsList(topicsList = DataSource.topics)
+    ItemsList(
+        topicsList = DataSource.topics,
+        modifier = Modifier.padding(
+            start = 16.dp,
+            top = 16.dp,
+            end = 16.dp,
+        )
+    )
 }
 
 @Composable
 fun GridItemCard (modifier: Modifier = Modifier, topic: Topic) {
-    Card(
-        modifier = modifier
-            .height(68.dp)
-    ) {
+    Card {
         Row {
-            Image(
-                painter = painterResource(id = topic.topicImage),
-                contentDescription = null,
+            Box {
+                Image(
+                    painter = painterResource(id = topic.topicImage),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
                 )
-            Column (
-
-
-            ) {
+            }
+            Column{
                 Text(
-                    text = stringResource(id = topic.topicId)
+                    text = stringResource(id = topic.topicId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        start = 10.dp,
+                        top = 10.dp,
+                        end = 10.dp,
+                        bottom = 5.dp
+                    )
                 )
-                Row {
+                Row (verticalAlignment = Alignment.CenterVertically){
                     Icon(
                         Icons.Rounded.Add,
-                        contentDescription = null
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 10.dp)
                     )
-                    Text(text= topic.numberOfCourses.toString())
+                    Text(
+                        text= topic.numberOfCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = 5.dp)
+                    )
                 }
             }
         }
@@ -90,8 +118,8 @@ fun GridItemCard (modifier: Modifier = Modifier, topic: Topic) {
 fun ItemsList(topicsList: List<Topic>,modifier: Modifier = Modifier) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
         modifier = modifier
     ) {
         items(topicsList) { topic ->
